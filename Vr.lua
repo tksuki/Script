0566f76bf868a8eea2ebc0cbf993cce7ab10023f
Tkdaisuki <morimoto.shun@icloud.com>
2026-04-29 21:05:17 +0900

    Update Vr.lua

--- Vr.lua
+++ Vr.lua
@@ -6,11 +6,23 @@ local raw = game:HttpGet(
 
 local data = HttpService:JSONDecode(raw)
 
-local c = 0
+local map = {
+ [60]="A",
+ [61]="W",
+ [62]="S",
+ [63]="E",
+ [64]="D",
+ [65]="F",
+ [66]="T",
+ [67]="G",
+ [68]="Y",
+ [69]="H",
+ [70]="U",
+ [71]="J",
+ [72]="K"
+}
 
 for _,note in ipairs(data.tracks[1].notes) do
-    print(note.midi, note.time)
-
-    c = c + 1
-    if c >= 10 then break end
+    print(map[note.midi])
+    break
 end
